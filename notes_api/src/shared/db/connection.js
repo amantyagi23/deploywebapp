77fca232858e7d35afc8cf08f63d5f1705a8e4cc
amantyagi23 <119286791+amantyagi23@users.noreply.github.com>
2023-07-30 13:19:06 +0530

    Update connection.js

--- notes_api/src/shared/db/connection.js
+++ notes_api/src/shared/db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 //console.log('DB URL is ', process.env.DBURL);
 export function createConnection(){
-const promise = mongoose.connect(mongodb+srv://test:test123@cluster0.y6jwf5b.mongodb.net/notetakingappdb?retryWrites=true&w=majority,{maxPoolSize:5});
+const promise = mongoose.connect('mongodb+srv://test:test123@cluster0.y6jwf5b.mongodb.net/notetakingappdb?retryWrites=true&w=majority',{maxPoolSize:5});
     promise.then(data=>{
         console.log('DB URL is mongodb+srv://test:test123@cluster0.y6jwf5b.mongodb.net/notetakingappdb?retryWrites=true&w=majority');
         console.log('Connection Created...');
